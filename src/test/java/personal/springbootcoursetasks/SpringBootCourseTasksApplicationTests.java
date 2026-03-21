1e8ac075f17423f81585cb36fb479cbf467424a5
Nijat Nasirli <nicat.nasirli0511@gmail.com>
2026-03-21 19:36:42 +0400

    Tasks related to spring boot

---
+++ src/test/java/personal/springbootcoursetasks/SpringBootCourseTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.springbootcoursetasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseTasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
